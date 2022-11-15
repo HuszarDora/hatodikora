98ce5c3d682ea756f4254f65a3d569d0bc743b79
HuszarDora <dora.huszar@stud.uni-corvinus.hu>
2022-11-15 11:20:24 +0100

    opcio arazas

--- nyolc.py
+++ nyolc.py
@@ -29,6 +29,17 @@ df0=df[df.index<"2018-03-01"]
 df0.loc[:,"implied_vola_mid"]=df0.apply(calcVolaMid,axis=1)
 print(df0)
 
+# symbol="KO 180323C48000"
+# df=df0[df0.symbol==symbol]
+
+dates=df0.index.unique()
+
+df_=df0[df0.index==dates[23]]
+df_=df0[df0.daysToExp==102]
+date=df_.index[0]
+df_=df_[df_.last_date==date]
+df_.groupby(df_.strike).implied_vola_mid.median().plot()
+plt.show()
 # def calcVolaBid(row):
 #     opt = Option(row.cp_flag, row.strike, row.expiry, 1)
 #     if row

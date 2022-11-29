6d9c40201074bc6ea0eee2b4b9d761738e21e0c1
HuszarDora <dora.huszar@stud.uni-corvinus.hu>
2022-11-29 09:36:07 +0100

    portfolio

---
+++ kilencedik_ora/PortfolioOptimizer.py
@@ -0,0 +1,11 @@
+class PortfolioOptimizer:
+    def __init__(self),asset_price_file_loc:
+        pass
+
+    def uj_fv(self):
+
+
+
+
+    if __name__=="__main__":
+        P0 =PortfolioOptimizer("Price_history.csv")
